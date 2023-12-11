ab54a8ef1bfd9e955f156ae31038e254cdd3ea1d
sanjaymarathe <itsmesmarathe@gmail.com>
2023-12-10 23:46:48 -0800

    first commit

---
+++ db.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql')
+
+
+
+const db = mysql.createConnection({
+    host: 'database-1.cpxfz0aupufi.us-east-1.rds.amazonaws.com',
+    port: '3306',
+    user: 'admin',
+    password: 'Smarathe!00',
+    database: 'emp_database',
+})
+
+
+
+
+module.exports = async () => {
+    db.connect(err => {
+        if (err) {
+            console.log(err.message)
+            return
+        }
+        console.log('db connected')
+    })
+}
+
+
---
+++ index.js
@@ -0,0 +1,39 @@
+const connectToDatabase = require("./db");
+const sequelize = require('sequelize');
+
+
+module.exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.healthCheck = async () => {
+  try {
+    await connectToDatabase();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Connection successful." }),
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: {
+        "Content-Type": "text/plain",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify({
+        error: err.message || "Connection is not successful.",
+      }),
+    };
+  }
+};
